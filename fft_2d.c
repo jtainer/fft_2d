c5fa8bf1d1d056e5fc908a9aafc8da2ea5c4b6d6
jtain <jtain@debian>
2023-03-02 03:34:12 -0500

    initial implementation of 2d fft

--- fft_2d.c
+++ fft_2d.c
@@ -21,7 +21,31 @@ void destroy_surface_complex(surface_complex surface) {
 }
 
 void fft_2d(surface_complex surface) {
+	// 1D transform on each row
+	unsigned int n = surface.width;
+	float complex* sig = surface.data;
+	for (unsigned int y = 0; y < surface.height; y++) {
+		fft_inpl(sig, n);
+		sig += n;
+	}
 
+	// 1D transform on each column
+	n = surface.height;
+	float complex* tmp = malloc(sizeof(float complex) * n);
+	for (unsigned int x = 0; x < surface.width; x++) {
+		// Column elements are not adjacent in memory so they must be
+		// copied into a temporary buffer
+		for (unsigned int y = 0; y < n; y++) {
+			tmp[y] = surface.data[y * surface.width + x];
+		}
+		// Transform signal in temp buffer
+		fft_inpl(tmp, n);
+		// Write temp buffer back to original surface
+		for (unsigned int y = 0; y < n; y++) {
+			surface.data[y * surface.width + x] = tmp[y];
+		}
+	}
+	free(tmp);
 }
 
 void ifft_2d(surface_complex surface) {
