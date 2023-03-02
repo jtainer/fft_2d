5c771a07fd7c5857ad4510f8e91e8780510dbac4
jtain <jtain@debian>
2023-03-02 00:34:43 -0500

    too tired to come up with commit message, going to sleep

---
+++ fft_2d.h
@@ -0,0 +1,21 @@
+// 
+// 2-dimensional FFT for image processing
+//
+// 2023, Jonathan Tainer
+//
+
+#ifndef FFT_2D_H
+#define FFT_2D_H
+
+#include "fft/fft.h"
+
+typedef struct surface_complex {
+	unsigned int width;
+	unsigned int height;
+	float complex* data;
+} surface_complex;
+
+surface_complex create_surface_complex(unsigned int width, unsigned int height);
+void destroy_surface_complex(surface_complex surface);
+
+#endif
